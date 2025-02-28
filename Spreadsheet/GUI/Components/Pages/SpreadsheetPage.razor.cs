636835d6ca814101c687b5cb3c4816d83e29069c
leocodingg <132714535+leocodingg@users.noreply.github.com>
2025-02-28 10:26:52 -0700

    Setup the blazor gui and added reference for the GUI project

---
+++ Spreadsheet/GUI/Components/Pages/SpreadsheetPage.razor.cs
@@ -0,0 +1,106 @@
+﻿// <copyright file="SpreadsheetPage.razor.cs" company="UofU-CS3500">
+// Copyright (c) 2024 UofU-CS3500. All rights reserved.
+// </copyright>
+
+namespace GUI.Client.Pages;
+
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics;
+
+/// <summary>
+/// TODO: Fill in
+/// </summary>
+public partial class SpreadsheetPage
+{
+    /// <summary>
+    /// Based on your computer, you could shrink/grow this value based on performance.
+    /// </summary>
+    private const int ROWS = 50;
+
+    /// <summary>
+    /// Number of columns, which will be labeled A-Z.
+    /// </summary>
+    private const int COLS = 26;
+
+    /// <summary>
+    /// Provides an easy way to convert from an index to a letter (0 -> A)
+    /// </summary>
+    private char[] Alphabet { get; } = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+
+    /// <summary>
+    ///   Gets or sets the name of the file to be saved
+    /// </summary>
+    private string FileSaveName { get; set; } = "Spreadsheet.sprd";
+
+
+    /// <summary>
+    ///   <para> Gets or sets the data for all of the cells in the spreadsheet GUI. </para>
+    ///   <remarks>Backing Store for HTML</remarks>
+    /// </summary>
+    private string[,] CellsBackingStore { get; set; } = new string[ROWS, COLS];
+
+
+    /// <summary>
+    /// Handler for when a cell is clicked
+    /// </summary>
+    /// <param name="row">The row component of the cell's coordinates</param>
+    /// <param name="col">The column component of the cell's coordinates</param>
+    private void CellClicked( int row, int col )
+    {
+
+    }
+
+
+    /// <summary>
+    /// Saves the current spreadsheet, by providing a download of a file
+    /// containing the json representation of the spreadsheet.
+    /// </summary>
+    private async void SaveFile()
+    {
+        await JSRuntime.InvokeVoidAsync( "downloadFile", FileSaveName, 
+            "replace this with the json representation of the current spreadsheet" );
+    }
+
+    /// <summary>
+    /// This method will run when the file chooser is used, for loading a file.
+    /// Uploads a file containing a json representation of a spreadsheet, and 
+    /// replaces the current sheet with the loaded one.
+    /// </summary>
+    /// <param name="args">The event arguments, which contains the selected file name</param>
+    private async void HandleFileChooser( EventArgs args )
+    {
+        try
+        {
+            string fileContent = string.Empty;
+
+            InputFileChangeEventArgs eventArgs = args as InputFileChangeEventArgs ?? throw new Exception("unable to get file name");
+            if ( eventArgs.FileCount == 1 )
+            {
+                var file = eventArgs.File;
+                if ( file is null )
+                {
+                    return;
+                }
+
+                using var stream = file.OpenReadStream();
+                using var reader = new StreamReader(stream);
+
+                // fileContent will contain the contents of the loaded file
+                fileContent = await reader.ReadToEndAsync();
+
+                // TODO: Use the loaded fileContent to replace the current spreadsheet
+
+                StateHasChanged();
+            }
+        }
+        catch ( Exception e )
+        {
+            Debug.WriteLine( "an error occurred while loading the file..." + e );
+        }
+    }
+
+}
